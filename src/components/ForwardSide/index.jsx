1a02c7c62bcf35527e3e11a14281e3b85e2cbeb6
Maxim Korzenok <maximka00007777@gmail.com>
2022-04-21 15:30:38 +0300

    feat: implemented data caching

--- src/components/ForwardSide/index.jsx
+++ src/components/ForwardSide/index.jsx
@@ -19,58 +19,68 @@ import {
 const ForwardSide = () => {
   const [coords, setCoords] = useState({ lat: null, lon: null });
   const [city, setCity] = useState("");
+  const [currentTime, setCurrentTime] = useState(() => new Date().getTime());
   const { fetchOneDayWeather } = useActions();
   const { fetchFiveDaysWeather } = useActions();
+  const { fetchSelfLocationOneDayWeather } = useActions();
+  const { fetchSelfLocationFiveDaysWeather } = useActions();
+  const { setCurrentWeatherToView } = useActions();
+  const { setWeatherTime } = useActions();
+  const { refreshWeather } = useActions();
   const allData = useSelector(weatherSelector);
 
   const onSearchHandler = (e) => {
-    if (e.key === "Enter") {
+    if (
+      e.key === "Enter" &&
+      city.toLowerCase() !== allData.oneDayWeather?.name.toLowerCase()
+    ) {
       fetchOneDayWeather(city);
       fetchFiveDaysWeather(city);
+    } else if (e.key === "Enter" && null != false) {
+      setCurrentWeatherToView("search");
     }
   };
 
   const onChangeSearchHandler = (e) => setCity(e.target.value);
 
-  const getSelfLocation = () => {
-    const location = window.navigator && window.navigator.geolocation;
-
-    if (location) {
-      location.getCurrentPosition(
-        (position) => {
-          setCoords({
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-          });
-        },
-        (error) => {
-          setCoords({ lat: "err-latitude", lon: "err-longitude" });
-        }
-      );
+  useEffect(() => {
+    if (coords.lat !== null && !allData.selfLocationOneDayWeather) {
+      fetchSelfLocationOneDayWeather(coords.lat, coords.lon);
+      fetchSelfLocationFiveDaysWeather(coords.lat, coords.lon);
     }
-  };
+  }, [coords]);
 
   useEffect(() => {
+    if (allData.selfLocationOneDayWeather) setCurrentWeatherToView("self");
+  }, [window.location, allData.selfLocationOneDayWeather]);
+
+  useEffect(() => {
+    const getSelfLocation = () => {
+      const location = window.navigator && window.navigator.geolocation;
+
+      if (location) {
+        location.getCurrentPosition(
+          (position) => {
+            setCoords({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude,
+            });
+          },
+          (error) => {
+            setCoords({ lat: "err-latitude", lon: "err-longitude" });
+          }
+        );
+      }
+    };
+
+    const time = currentTime;
+    if (!allData.weatherTime) setWeatherTime(time);
+    if (currentTime - allData.weatherTime > 3600000 && allData.weatherTime) {
+      refreshWeather();
+      setWeatherTime(time);
+    }
     getSelfLocation();
   }, []);
-
-  useEffect(() => {
-    if (coords.lat !== null) {
-      // axios
-      //   // .get(
-      //   //   `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${process.env.REACT_APP_API_KEY}`
-      //   // )
-      //   // .get(
-      //   //   `https://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&appid=${process.env.REACT_APP_API_KEY}`
-      //   // )
-      //   .then((response) => {
-      //     console.log(response.data);
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
-    }
-  }, [allData]);
 
   return (
     <PageWrapper>
@@ -84,20 +94,20 @@ const ForwardSide = () => {
             onKeyDown={onSearchHandler}
           />
         </SearchBox>
-        {allData.oneDayWeather !== null ? (
+        {allData.currentWeatherOneDay !== null ? (
           <div>
             <LocationWrapper>
               <Location>
-                {allData.oneDayWeather.name},{" "}
-                {allData.oneDayWeather.sys.country}
+                {allData.currentWeatherOneDay.name},{" "}
+                {allData.currentWeatherOneDay.sys.country}
               </Location>
               <DateWrapper>{dateBuilder(new Date())}</DateWrapper>
             </LocationWrapper>
             <WeatherWrapper>
               <Temperature>
-                {Math.round(allData.oneDayWeather.main.temp)}°С
+                {Math.round(allData.currentWeatherOneDay.main.temp)}°С
               </Temperature>
-              <Weather>{allData.oneDayWeather.weather[0].main}</Weather>
+              <Weather>{allData.currentWeatherOneDay.weather[0].main}</Weather>
             </WeatherWrapper>
           </div>
         ) : (
--- src/store/actionCreators/weather.js
+++ src/store/actionCreators/weather.js
@@ -6,7 +6,21 @@ import {
   FETCH_ONE_DAY_WEATHER,
   FETCH_ONE_DAY_WEATHER_ERROR,
   FETCH_ONE_DAY_WEATHER_SUCCESS,
+  FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER,
+  FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER_ERROR,
+  FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER_SUCCESS,
+  FETCH_SELF_LOCATION_ONE_DAY_WEATHER,
+  FETCH_SELF_LOCATION_ONE_DAY_WEATHER_ERROR,
+  FETCH_SELF_LOCATION_ONE_DAY_WEATHER_SUCCESS,
+  REFRESH_WEATHER,
+  SET_CURRENT_WEATHER_TO_VIEW,
+  SET_WEATHER_TIME,
 } from "../constants";
+
+export const setCurrentWeatherToView = (condition) => ({
+  type: SET_CURRENT_WEATHER_TO_VIEW,
+  payload: condition,
+});
 
 export const fetchOneDayWeather = (city) => {
   return async (dispatch) => {
@@ -19,6 +33,7 @@ export const fetchOneDayWeather = (city) => {
         type: FETCH_ONE_DAY_WEATHER_SUCCESS,
         payload: response.data,
       });
+      dispatch(setCurrentWeatherToView("search"));
     } catch (e) {
       dispatch({
         type: FETCH_ONE_DAY_WEATHER_ERROR,
@@ -47,3 +62,52 @@ export const fetchFiveDaysWeather = (city) => {
     }
   };
 };
+
+export const fetchSelfLocationOneDayWeather = (lat, lon) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: FETCH_SELF_LOCATION_ONE_DAY_WEATHER });
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      dispatch({
+        type: FETCH_SELF_LOCATION_ONE_DAY_WEATHER_SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: FETCH_SELF_LOCATION_ONE_DAY_WEATHER_ERROR,
+        payload: "Error loading one day self location weather",
+      });
+    }
+  };
+};
+
+export const fetchSelfLocationFiveDaysWeather = (lat, lon) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER });
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      dispatch({
+        type: FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER_SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: FETCH_SELF_LOCATION_FIVE_DAYS_WEATHER_ERROR,
+        payload: "Error loading five days self location weather",
+      });
+    }
+  };
+};
+
+export const setWeatherTime = (time) => ({
+  type: SET_WEATHER_TIME,
+  payload: time,
+});
+
+export const refreshWeather = () => ({
+  type: REFRESH_WEATHER,
+});
